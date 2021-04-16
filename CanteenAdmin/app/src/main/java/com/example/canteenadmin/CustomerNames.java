ef13568d6e3e1ddcfc3c2c5055dd45d1a7be7903
N S Gainda <139-nsg5@users.noreply.campus.cs.le.ac.uk>
2021-04-16 14:31:17 +0000

    Upload New File

---
+++ CanteenAdmin/app/src/main/java/com/example/canteenadmin/CustomerNames.java
@@ -0,0 +1,17 @@
+package com.example.canteenadmin;
+
+import java.util.List;
+
+public class CustomerNames {
+    private String customerName;
+    private List<OrderDetail> orderDetails;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+}
