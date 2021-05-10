88a9211253aff29649dbee5dbe6cd4c0599ea7cc
N S Gainda <139-nsg5@users.noreply.campus.cs.le.ac.uk>
2021-05-10 05:30:54 +0100

    Added logout from MealDetailActivity

--- app/src/main/java/com/example/canteen/MealDetailActivity.java
+++ app/src/main/java/com/example/canteen/MealDetailActivity.java
@@ -3,24 +3,20 @@ package com.example.canteen;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
-
-import java.util.ArrayList;
 
 public class MealDetailActivity extends AppCompatActivity {
 
@@ -39,45 +35,42 @@ public class MealDetailActivity extends AppCompatActivity {
         String mealPrice = intent.getStringExtra("mealPrice");
         String imageUrl = intent.getStringExtra("mealImage");
         String mealIngredients = intent.getStringExtra("mealIngredients");
+        String universityInitials = intent.getStringExtra("universityInitials");
         String email = intent.getStringExtra("email");
 
-        Log.d("TAG", "onCreate: " + email);
-
         //Getting the views from the activity_meal_detail
-        TextView name = (TextView) findViewById(R.id.meal_name);
-        TextView description = (TextView) findViewById(R.id.meal_desc);
-        TextView price = (TextView) findViewById(R.id.meal_price);
-        ImageView image = (ImageView) findViewById(R.id.meal_image);
-        TextView mealid = (TextView) findViewById(R.id.meal_id);
-        TextView ingredients = (TextView) findViewById(R.id.meal_ingredients);
+        TextView nameView = (TextView) findViewById(R.id.meal_name);
+        TextView descriptionView = (TextView) findViewById(R.id.meal_desc);
+        TextView priceView = (TextView) findViewById(R.id.meal_price);
+        ImageView imageView = (ImageView) findViewById(R.id.meal_image);
+        TextView mealIdView = (TextView) findViewById(R.id.meal_id);
+        TextView ingredientsView = (TextView) findViewById(R.id.meal_ingredients);
 
         //Attaching data to the view
-        name.setText(mealName);
-        name.setTextColor(Color.rgb(98, 0, 238));
-        description.setText(mealDescription);
-        price.setText("£" + String.valueOf(mealPrice));
-        mealid.setText(mealId);
-        Picasso.get().load(imageUrl).into(image);
-        ingredients.setText(mealIngredients);
+        nameView.setText(mealName);
+        nameView.setTextColor(Color.rgb(98, 0, 238));
+        descriptionView.setText(mealDescription);
+        priceView.setText("£" + mealPrice);
+        mealIdView.setText(mealId);
+        Picasso.get().load(imageUrl).into(imageView);
+        ingredientsView.setText(mealIngredients);
 
-        // Declare buttons
+        // Declare quantity and buttons
         TextView quantity = (TextView) findViewById(R.id.quantity);
         Button increaseQuantity = (Button) findViewById(R.id.increase_quantity);
         Button decreaseQuantity = (Button) findViewById(R.id.decrease_quantity);
         Button addToBasket = (Button) findViewById(R.id.addToBasket);
-        Button clearBasket = (Button) findViewById(R.id.clear_basket);
 
         // Handle Button Increase Click
         increaseQuantity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int newQuantity = Integer.parseInt(quantity.getText().toString());
-                newQuantity = newQuantity + 1;
-                quantity.setText(String.valueOf(newQuantity));
+                int mealQuantity = Integer.parseInt(quantity.getText().toString());
+                mealQuantity = mealQuantity + 1;
+                quantity.setText(String.valueOf(mealQuantity));
 
-                String priceString = String.valueOf(newQuantity * Float.parseFloat(mealPrice));
-                price.setText("£" + priceString);
-
+                String totalPrice = String.valueOf(mealQuantity * Float.parseFloat(mealPrice));
+                priceView.setText("£" + totalPrice);
             }
         });
 
@@ -85,56 +78,49 @@ public class MealDetailActivity extends AppCompatActivity {
         decreaseQuantity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int newQuantity = Integer.parseInt(quantity.getText().toString());
-                if (newQuantity > 1) {
-                    newQuantity = newQuantity - 1;
-                    quantity.setText(String.valueOf(newQuantity));
+                int mealQuantity = Integer.parseInt(quantity.getText().toString());
+                if (mealQuantity > 1) {
+                    mealQuantity = mealQuantity - 1;
+                    quantity.setText(String.valueOf(mealQuantity));
 
-                    String priceString = String.valueOf(newQuantity * Float.parseFloat(mealPrice));
-                    price.setText("£" + priceString);
+                    String totalPrice = String.valueOf(mealQuantity * Float.parseFloat(mealPrice));
+                    priceView.setText("£" + totalPrice);
                 }
             }
         });
 
-        ref = FirebaseDatabase.getInstance().getReference("/customer");
-
-        addToBasket.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                ref.addValueEventListener(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                        if (snapshot.exists()) {
-//                            for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
-//                                String name = dataSnapshot.child("email").getValue().toString();
-//
-//                                if (name.equals(email)) {
-//                                    Log.d("TAG", "onDataChange: " + name);
-//
-//                                    Basket basket = new Basket();
-//                                    basket.setMealID(mealId);
-//                                    basket.setName(mealName);
-//                                    basket.setDescription(mealDescription);
-//                                    basket.setIngredients(mealIngredients);
-//                                    basket.setPrice(mealPrice);
-//                                    basket.setQuantity(quantity.getText().toString());
-//                                    basket.setUrl(imageUrl);
-//
-//                                    ref.child(dataSnapshot.getKey()).child("basket").child("meal" + mealId).setValue(basket);
-//
-//                                }
-//
-//
+//        //Calculating stock;
+//        //If there is stock, then make button active.
+//        //Else, make button inactive
+//        DatabaseReference mealRef = FirebaseDatabase.getInstance().getReference("/meals");
+//        mealRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if (snapshot.exists()) {
+//                    for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
+//                        String dataMealID = dataSnapshot.child("mealID").getValue().toString();
+//                        if (dataMealID.equals(mealId)) {
+//                            String stock = dataSnapshot.child("stock").getValue().toString();
+//                            if (Integer.parseInt(stock) > 0) {
+//                                addToBasket.setEnabled(true);
+//                            } else {
+//                                addToBasket.setEnabled(false);
 //                            }
 //                        }
 //                    }
+//                }
+//            }
 //
-//                    @Override
-//                    public void onCancelled(@NonNull DatabaseError error) {
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
 //
-//                    }
-//                });
+//            }
+//        });
 
+        ref = FirebaseDatabase.getInstance().getReference("/customer");
+        addToBasket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Basket basket = new Basket();
                 basket.setMealID(mealId);
                 basket.setName(mealName);
@@ -144,54 +130,44 @@ public class MealDetailActivity extends AppCompatActivity {
                 basket.setQuantity(quantity.getText().toString());
                 basket.setUrl(imageUrl);
 
-
-
-
-                ref.child(email.substring(0, 4)).child("basket").child("meal" + mealId).setValue(basket);
+                ref.child(universityInitials).child("basket").child("meal" + mealId).setValue(basket);
                 Toast.makeText(getApplicationContext(), "MEAL ADDED", Toast.LENGTH_SHORT).show();
 
                 Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                intent.putExtra("universityInitials", universityInitials);
                 intent.putExtra("email", email);
                 startActivity(intent);
             }
         });
+    }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout_menu, menu);
 
+        MenuItem logout = menu.findItem(R.id.logoutFromActivity);
 
+        Intent intent = getIntent();
+        String universityInitials = intent.getStringExtra("universityInitials");
+        String email = intent.getStringExtra("email");
 
+        logout.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                FirebaseAuth.getInstance().signOut();
 
+                //Clear basket upon logout
+                DatabaseReference customerRef = FirebaseDatabase.getInstance().getReference("/customer");
+                customerRef.child(universityInitials).child("basket").removeValue();
 
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                System.exit(0);
+                return true;
+            }
+        });
 
-
-
-
-//        ref = FirebaseDatabase.getInstance().getReference("/customer");
-//
-//        addToBasket.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                Basket basket = new Basket();
-//                basket.setMealID(mealId);
-//                basket.setName(mealName);
-//                basket.setDescription(mealDescription);
-//                basket.setIngredients(mealIngredients);
-//                basket.setPrice(mealPrice);
-//                basket.setQuantity(quantity.getText().toString());
-//                basket.setUrl(imageUrl);
-//
-//                Log.d("TAG", "onClick:  " + email);
-//
-//
-//                ref.child(email.substring(0, 4)).child("basket").child("meal" + mealId).setValue(basket);
-//                Toast.makeText(getApplicationContext(), "MEAL ADDED", Toast.LENGTH_SHORT).show();
-//
-//                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
-//                intent.putExtra("email", email);
-//                startActivity(intent);
-//            }
-//        });
-
+        return super.onCreateOptionsMenu(menu);
     }
 }
