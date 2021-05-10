21745ce1e9b963c138773e4b898085e93018b171
N S Gainda <139-nsg5@users.noreply.campus.cs.le.ac.uk>
2021-05-10 05:16:46 +0100

    Updated AwaitingOrderActivity

--- app/src/main/java/com/example/canteen/AwaitingOrderActivity.java
+++ app/src/main/java/com/example/canteen/AwaitingOrderActivity.java
@@ -1,25 +1,15 @@
 package com.example.canteen;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class AwaitingOrderActivity extends AppCompatActivity {
 
@@ -35,24 +25,23 @@ public class AwaitingOrderActivity extends AppCompatActivity {
         //Getting universityInitials
         Intent intent = getIntent();
         String universityInitials = intent.getStringExtra("universityInitials");
-        ///
 
-//        //Handle logoutwwwjjjjjj
-//        Button logout = (Button) findViewById(R.id.logout);
-//        logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                fAuth.signOut();
-//
-//                //Clear basket upon logout
-//                DatabaseReference customerRef = FirebaseDatabase.getInstance().getReference("/customer");
-//                customerRef.child(universityInitials).child("basket").removeValue();
-//
-//                Intent intent = new Intent(AwaitingOrderActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//                System.exit(0);
-//            }
-//        });
+        //Handle logout
+        Button logout = (Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fAuth.signOut();
+
+                //Clear basket upon logout
+                DatabaseReference customerRef = FirebaseDatabase.getInstance().getReference("/customer");
+                customerRef.child(universityInitials).child("basket").removeValue();
+
+                Intent intent = new Intent(AwaitingOrderActivity.this, LoginActivity.class);
+                startActivity(intent);
+                System.exit(0);
+            }
+        });
 
     }
 
