8b47147a426e07a59fb5f21c7532baa3cd8b9eeb
N S Gainda <139-nsg5@users.noreply.campus.cs.le.ac.uk>
2021-05-10 02:40:21 +0000

    Update AwaitingOrderActivity.java - adding logout button

--- app/src/main/java/com/example/canteen/AwaitingOrderActivity.java
+++ app/src/main/java/com/example/canteen/AwaitingOrderActivity.java
@@ -1,15 +1,25 @@
 package com.example.canteen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AwaitingOrderActivity extends AppCompatActivity {
 
@@ -22,38 +32,32 @@ public class AwaitingOrderActivity extends AppCompatActivity {
 
         fAuth = FirebaseAuth.getInstance();
 
-
+        //Getting universityInitials
         Intent intent = getIntent();
-        String email = intent.getStringExtra("email");
+        String universityInitials = intent.getStringExtra("universityInitials");
 
-        Log.d("AwaitingOrder", "onCreate: " + email);
-
-
+        //Handle logout
         Button logout = (Button) findViewById(R.id.logout);
-
-
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fAuth.signOut();
-                signOutUser();
+
+                //Clear basket upon logout
+                DatabaseReference customerRef = FirebaseDatabase.getInstance().getReference("/customer");
+                customerRef.child(universityInitials).child("basket").removeValue();
+
+                Intent intent = new Intent(AwaitingOrderActivity.this, LoginActivity.class);
+                startActivity(intent);
+                System.exit(0);
             }
         });
 
     }
 
-
     @Override
     public void onBackPressed() {
-
-    }
-
-
-    private void signOutUser() {
-        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-        finish();
+        //Nothing happens
     }
 
 }
