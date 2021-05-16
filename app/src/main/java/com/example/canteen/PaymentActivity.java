3872de7ca1f2eb81bd28a14d394ff37f4a1adb15
N S Gainda <139-nsg5@users.noreply.campus.cs.le.ac.uk>
2021-05-16 23:47:50 +0100

    Adding citations for PaymentActivity

--- app/src/main/java/com/example/canteen/PaymentActivity.java
+++ app/src/main/java/com/example/canteen/PaymentActivity.java
@@ -6,12 +6,18 @@ import com.stripe.android.PaymentConfiguration;
 
 public class PaymentActivity extends Application {
 
-        @Override
-        public void onCreate() {
-            super.onCreate();
-            PaymentConfiguration.init(
-                    getApplicationContext(),
-                    "pk_test_51IHfbrGfF6sAEAR9wvJrBW61soe8DfrRc5hKyQsl8SkfimyT0rolH6bj2fLDwD3qm202aIh62SKZilBCE99xTYid00pAUA3uRR"
-            );
-        }
+    /*
+        This sample of code has been taken from:
+        https://stripe.com/docs/payments/accept-a-payment?platform=android
+
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PaymentConfiguration.init(
+                getApplicationContext(),
+                "pk_test_51IHfbrGfF6sAEAR9wvJrBW61soe8DfrRc5hKyQsl8SkfimyT0rolH6bj2fLDwD3qm202aIh62SKZilBCE99xTYid00pAUA3uRR"
+        );
+    }
+
 }
